bd83523834449bbaeccf98b25b256a812588dce9
Pullolian <floriancunat@orange.fr>
2020-01-20 19:31:14 +0100

    jsbgfjshfq

--- PetSylvanie/src/main/java/fr/solutec/dao/AlerteRepository.java
+++ PetSylvanie/src/main/java/fr/solutec/dao/AlerteRepository.java
@@ -1,8 +1,14 @@
 package fr.solutec.dao;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
 import fr.solutec.entities.Alerte;
 
 public interface AlerteRepository extends CrudRepository<Alerte, Long>{
-
+	
+	@Query(value = "SELECT warn FROM Alerte warn")
+	public List<Alerte> getAllAlerte();
 }
---
+++ PetSylvanie/src/main/java/fr/solutec/rest/AlerteRest.java
@@ -0,0 +1,24 @@
+package fr.solutec.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.solutec.dao.AlerteRepository;
+import fr.solutec.entities.Alerte;
+
+@RestController @CrossOrigin("*")
+public class AlerteRest {
+
+	@Autowired 
+	private AlerteRepository alerteRepo;
+	
+	@RequestMapping(value = "/alerte", method = RequestMethod.GET)
+	public List<Alerte> getAllAlerte(){		
+		return (List<Alerte>)alerteRepo.findAll();
+	}
+}
